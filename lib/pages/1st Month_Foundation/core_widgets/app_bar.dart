15ae9a2d04923c91e47cbf0035f5bc19bc55ea3b
Dev-hurl <adekanmbiemmanuel247@gmail.com>
2026-04-17 13:51:33 +0100

    Restructured Pages

--- lib/pages/1st Month_Foundation/core_widgets/app_bar.dart
+++ lib/pages/1st Month_Foundation/core_widgets/app_bar.dart
@@ -40,6 +40,7 @@ class ApplicationBar extends StatelessWidget {
         child: Padding(
           padding: EdgeInsets.all(16.0),
           child: Column(
+            crossAxisAlignment:CrossAxisAlignment.center,
             children: [
               Text(
                 'This is the body of the application.',
