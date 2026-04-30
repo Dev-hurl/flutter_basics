0568ecbe9be6a50acedc94ccc0a901de9eb5547c
BuildwithNuel <devhurl7@gmail.com>
2026-04-30 01:49:18 +0100

    fixed stuff

---
+++ lib/pages/2 UI/MoreWidgets/switch_radio_slider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SwitchRadioSlider extends StatelessWidget {
+  const SwitchRadioSlider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
