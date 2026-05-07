93bd649b8084c0a60454385b9b400819ec9ed39c
BuildwithNuel <devhurl7@gmail.com>
2026-05-07 02:20:54 +0100

    added new lines

---
+++ lib/pages/3 State Management/provider_mgmt.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProviderMgmt extends StatelessWidget {
+  const ProviderMgmt({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
